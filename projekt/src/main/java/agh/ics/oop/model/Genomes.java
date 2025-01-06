833b7eb72fd1f264704576fc45496926f51abb01
defalt <klwid04@gmail.com>
2025-01-06 20:52:42 +0100

    genomesss

--- projekt/src/main/java/agh/ics/oop/model/Genomes.java
+++ projekt/src/main/java/agh/ics/oop/model/Genomes.java
@@ -1,30 +1,56 @@
 package agh.ics.oop.model;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class Genomes {
     private int activeGenom;
-    private List<Integer> genomes;
+    private List<Integer> genomes = new ArrayList<>();
+    private Random random = new Random();
     private int n;
     private SimulationConfig conf;
     public Genomes(int n) {
         this.n = n;
-        Random random = new Random();
         activeGenom = random.nextInt(n);
         for (int i = 0; i < n; i++) {
             genomes.add(random.nextInt(8));
         }
     }
 
-    public Genomes(Animal animal, Animal parent, SimulationConfig conf) {
+    public Genomes(Animal animal1, Animal animal2, SimulationConfig conf) {
         this.conf = conf;
+        Animal stronger = animal1.getEnergy() >= animal2.getEnergy() ? animal1  : animal2;
+        Animal weaker = animal1.getEnergy() < animal2.getEnergy() ? animal1  : animal2;
+        int strongerLength = (int) (conf.getGenomeLength() * ((double) stronger.getEnergy() / (stronger.getEnergy() + weaker.getEnergy())));
+        int weakerLength = conf.getGenomeLength() - strongerLength;
+        int i = random.nextInt(2);
+        if (i==0){
+                genomes.addAll(stronger.getGenome().getGenomes().subList(0,strongerLength));
+                genomes.addAll(weaker.getGenome().getGenomes().subList(0,weakerLength));
+        }
+        else {
+            genomes.addAll(weaker.getGenome().getGenomes().subList(0,strongerLength));
+            genomes.addAll(stronger.getGenome().getGenomes().subList(0,weakerLength));
+        }
+        mutate();
+
     }
 
-    public int mutate(){
+
+    public void mutate(){
+
+
 
     }
+    protected void mutateOneGenomeWithRandom(){
+        int i = random.nextInt(conf.getGenomeLength());
+        genomes.set(i,random.nextInt(8));
+    }
 
+    public List<Integer> getGenomes() {
+        return genomes;
+    }
 
     public int getActiveGenom() {
         int oldGenom = activeGenom;
