b594ddd426d13ba78f35caa8e6e5df022c025cf6
defalt <klwid04@gmail.com>
2025-01-06 22:19:46 +0100

    added box

---
+++ projekt/src/main/java/agh/ics/oop/model/Box.java
@@ -0,0 +1,28 @@
+package agh.ics.oop.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Box {
+    private final List<Animal> animals = new ArrayList<>();
+    private Plant plant;
+    public void addAnimal(Animal animal){
+        animals.add(animal);
+    }
+    public void removeAnimal(Animal animal){
+        animals.remove(animal);
+    }
+    public List<Animal> getAnimals(){
+        return animals;
+    }
+    public void setPlant(Plant plant){
+        this.plant = plant;
+    }
+    public Plant getPlant(){
+        return plant;
+    }
+    public boolean isEmpty(){
+        return animals.isEmpty() && plant == null;
+    }
+
+}
---
+++ projekt/src/main/java/agh/ics/oop/model/Plant.java
@@ -0,0 +1,24 @@
+package agh.ics.oop.model;
+
+public class Plant implements WorldElement {
+    private final Vector2d position;
+    private final int energy;
+    public Plant(Vector2d position,int energy) {
+        this.energy = energy;
+        this.position = position;
+    }
+    @Override
+    public Vector2d getPosition() {
+        return position;
+    }
+
+
+    public int getEnergy() {
+        return energy;
+    }
+
+    @Override
+    public String toString() {
+        return "*";
+    }
+}
